46cc99e085907a6d5f4a279e9fc64d8df0b7e47e
Ritika <68857101+ritika309@users.noreply.github.com>
2021-07-12 05:04:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ function compute()
     }
     else{
 
-            document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\> at an interest rate of "+rate+"%\<br\>You will receive an interest "+interest+" hence the total amount of "+amount+",\<br\>in the year "+year+"\<br\>"
+            document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\> at an interest rate of "+rate+"%\<br\>You will receive an interest "+interest+"%\<br\> hence the total amount of "+amount+",\<br\>in the year "+year+"\<br\>"
 
     }
 }
